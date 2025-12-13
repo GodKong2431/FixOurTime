91c0e2c73e0ea9ccb206d7d0ace82e43c29ad3b4
GodKong2431 <kizna10231@gmail.com>
2025-12-14 02:17:16 +0900

    [feat] 플레이어 프리팹 추가 및 기어 상호삭용 연결
    플레이어 프리팹 추가 및 기어 상호삭용 연결

--- Assets/Scripts/Trap/Gear/InteractableGear.cs
+++ Assets/Scripts/Trap/Gear/InteractableGear.cs
@@ -1,36 +1,39 @@
 using System.Collections;
 using UnityEngine;
+enum GearStateType
+{
+    GearIdleState,
+    GearMoveState
+}
 
 public class InteractableGear : MoveGear,IDamageable
 {
+    private GearStateType _currentState;
+    private bool _isTrigger = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _currentState = GearStateType.GearIdleState;
+    }
+    private void Update()
+    {
+        if (_currentState == GearStateType.GearMoveState)
+        {
+            MoveNextPoint();
+            if (ChangeNextPoint())
+            {
+                _isTrigger = !_isTrigger;
+                _currentState = GearStateType.GearIdleState;
+            }
+        }
+    }
     public void TakeDamage(float damage, float KnockbackForce, Vector3 hitPos)
     {
         HitGear();
     }
     public void HitGear()
     {
-        StartCoroutine(StartMove());
-        
-        //인덱스 더하기
-        _currentIndex += _wayDir;
-
-        //마지막 인덱스에 도착했거나 처음 인덱스에 도착했으면 방향 변경
-        if (_currentIndex >= _points.Length - 1 || _currentIndex <= 0)
-            _wayDir *= -1;
-
-        //목표 포인트 변경
-        _nextPoint = _points[_currentIndex];
-    }
-
-    
-
-    IEnumerator StartMove()
-    {
-        MoveNextPoint();
-
-        if(ChangeNextPoint())
-            yield break;
-
-        yield return null;
+        _currentState = GearStateType.GearMoveState;
     }
 }
