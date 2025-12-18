7b2f767d4e72ab975030c8b061b090a112e8cd6c
GodKong2431 <kizna10231@gmail.com>
2025-12-18 22:45:21 +0900

    [feat] 윈드 존 구현
    바람 이펙트랑 존에 들어가있는 플레이어 조금씩 뒤로 밀리는 기능 구현

--- Assets/Scripts/Trap/WindZone/WindZone.cs
+++ Assets/Scripts/Trap/WindZone/WindZone.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class WindZone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("바람 강도")]
+    [SerializeField] private float _windPower = 0.001f;
+    [SerializeField] private float _accelerationDuraition = 1;
+    [SerializeField] private float _maxAcceleration = 2;
 
-    // Update is called once per frame
-    void Update()
+    private float _windSpeed;
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if(collision.TryGetComponent(out Player _player))
+        {
+            if(!_player.IsGrounded)
+            {
+                float maxWindSpeed = _windPower * _maxAcceleration;
+
+                float accelPerFixedFrame = maxWindSpeed / _accelerationDuraition * Time.fixedDeltaTime;
+
+                _windSpeed = Mathf.MoveTowards(_windSpeed, maxWindSpeed, accelPerFixedFrame);
+
+                _player.Rb.linearVelocity = new Vector2(_windSpeed, _player.Rb.linearVelocity.y);
+
+                Debug.Log(_windSpeed);
+            }
+            else
+            {
+                _windSpeed = _windPower;
+            }
+        }
     }
 }
