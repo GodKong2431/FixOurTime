6d18e447d51949588513e8e54835599d5d258b79
GodKong2431 <kizna10231@gmail.com>
2025-12-23 19:55:36 +0900

    [feat] 보스 패턴 분리 및 암흑창 패턴 제작
    보스 패턴 분리 및 암흑창 패턴 제작

---
+++ Assets/Scripts/Boss/Devil/DevilDarkSpear/DevilDarkSpear.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+public class DevilDarkSpear : MonoBehaviour
+{
+    [Header("이동 속도")]
+    [SerializeField] private float _moveSpeed = 2f;
+
+    private Vector3 _startPos;
+    private Vector3 _endPos;
+
+    private Coroutine _moveCoroutine;
+
+    private void Start()
+    {
+        _startPos = transform.position;
+        float height = GetHeight();
+        _endPos = _startPos + Vector3.up * height;
+    }
+
+    private float GetHeight()
+    {
+        TryGetComponent<SpriteRenderer>(out var sr);
+        return sr.bounds.size.y;
+    }
+
+    public void AttackSpear()
+    {
+        if (_moveCoroutine != null) StopCoroutine(_moveCoroutine);
+        _moveCoroutine = StartCoroutine(MoveTo(_endPos));
+    }
+
+    public void ReturnSpear()
+    {
+        if (_moveCoroutine != null) StopCoroutine(_moveCoroutine);
+        _moveCoroutine = StartCoroutine(MoveTo(_startPos));
+    }
+
+    private IEnumerator MoveTo(Vector3 target)
+    {
+        while (Vector3.Distance(transform.position, target) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                target,
+                _moveSpeed * Time.deltaTime
+            );
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/UI/InGameUI/PlayerUI/PlayerUiConnector.cs
+++ Assets/Scripts/UI/InGameUI/PlayerUI/PlayerUiConnector.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 public class PlayerUiConnector : MonoBehaviour
 {
     [SerializeField] private PlayerHpUI _playerHpUI;
-    [SerializeField] private Player _player;
 
     private PlayerHpPresenter _plaerHpPresenter;
 
     private void Start()
     {    
-        _plaerHpPresenter = new PlayerHpPresenter(_player, _playerHpUI);
+        _plaerHpPresenter = new PlayerHpPresenter(GameManager.Instance.Player, _playerHpUI);
 
         
     }
