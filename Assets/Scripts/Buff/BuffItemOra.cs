e51ff84852acd519e1215e111c39ca2241658f2e
Dokkaeb <yj4705@gmail.com>
2025-12-24 17:23:16 +0900

    [fix] 버프 아이템들 이펙트 수정

--- Assets/Scripts/Buff/BuffItemOra.cs
+++ Assets/Scripts/Buff/BuffItemOra.cs
@@ -10,11 +10,17 @@ public class BuffItemOra : MonoBehaviour
     [Header("회전 설정")]
     [SerializeField] float _rotationSpeed = 30f;
 
+    [Header("부유 설정")]
+    [SerializeField] float _floatingDistance = 0.2f;  //위아래 움직이는 거리
+    [SerializeField] float _floatingSpeed = 2f;
+
     SpriteRenderer _spr;
+    Vector3 _startPos;
 
     private void Awake()
     {
         _spr = GetComponent<SpriteRenderer>();
+        _startPos = transform.position;
     }
 
     private void Update()
@@ -27,5 +33,8 @@ public class BuffItemOra : MonoBehaviour
         Color c = _spr.color;
         c.a = currentAlpha;
         _spr.color = c;
+
+        float floating = _startPos.y + Mathf.Sin(Time.time * _floatingSpeed) * _floatingDistance;
+        transform.parent.position = new Vector3(_startPos.x, floating, _startPos.z);
     }
 }
