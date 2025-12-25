7ef048c42b3709d914775d9d1d45ff959e3b9908
GodKong2431 <kizna10231@gmail.com>
2025-12-25 19:42:57 +0900

    [fix] 붕괴구역 수정
    붕괴구역 타일맵 감지해서 부수게하고 플레이어 닿으면9999데미지 들어가서 죽게함

--- Assets/Scripts/Trap/DestroyTower/DestroyArea.cs
+++ Assets/Scripts/Trap/DestroyTower/DestroyArea.cs
@@ -1,25 +1,65 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DestroyArea : MonoBehaviour
 {
-    [Header("Move")]
-    [SerializeField] float _riseSpeed = 2f;
+    [Header("이동 속도")]
+    [SerializeField] private float _moveSpeed = 0.5f;
 
-    [Header("References")]
-    public Transform _destroyLine;
+    [Header("대상 타일맵")]
+    [SerializeField] private Tilemap _tilemap;
 
-    public float TopY => _destroyLine.position.y;
+    private Collider2D _zoneCollider;
 
-    void Update()
+    private void Awake()
     {
-        transform.Translate(Vector2.up * _riseSpeed * Time.deltaTime);
+        _zoneCollider = GetComponent<Collider2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+    private void FixedUpdate()
     {
-        if (col.CompareTag("Player"))
+        transform.position += Vector3.up * _moveSpeed * Time.deltaTime;
+
+        DestroyTilesInZone();
+    }
+
+    private void DestroyTilesInZone()
+    {
+        Bounds bounds = _zoneCollider.bounds;
+
+        Vector3Int minCell = _tilemap.WorldToCell(bounds.min);
+        Vector3Int maxCell = _tilemap.WorldToCell(bounds.max);
+
+        for (int x = minCell.x; x <= maxCell.x; x++)
         {
-            Debug.Log("플레이어 사망");
+            for (int y = minCell.y; y <= maxCell.y; y++)
+            {
+                Vector3Int cellPos = new Vector3Int(x, y, 0);
+
+                if (!_tilemap.HasTile(cellPos))
+                    continue;
+
+                // 타일 제거
+                _tilemap.SetTile(cellPos, null);
+            }
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<TilemapCollider2D>())
+            return;
+
+        if(other.GetComponent<ItemObject>())
+            _moveSpeed *= 2;
+
+        if (other.TryGetComponent(out Player player))
+        {
+            player.TakeDamage(9999, 0, transform.position);
+            return;
+        }
+            
+
+        other.gameObject.SetActive(false);
     }
 }
