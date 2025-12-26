42932468718f628a41c09a5830b2630d7864a35f
GodKong2431 <kizna10231@gmail.com>
2025-12-26 12:01:34 +0900

    [fix] 책장 버그 수정 및 보스존 카메라 연결

--- Assets/Scripts/Trap/BookCase/BookCaseState/BookCaseIdleState.cs
+++ Assets/Scripts/Trap/BookCase/BookCaseState/BookCaseIdleState.cs
@@ -14,7 +14,7 @@ public class BookCaseIdleState : IState<BookCase>
 
     public void Execute(BookCase context)
     {
-        Vector2 origin = new Vector2(context.Rb.position.x, context.BookCaseBotton);
+        Vector2 origin = new Vector2(context.Rb.position.x, context.BookCaseBottom);
         Vector2 RaySize = new Vector2(40, 1);
 
         RaycastHit2D[] BoxHits = Physics2D.BoxCastAll(origin, RaySize, 0f, Vector2.down, 0.2f, 1 << 10);
@@ -23,7 +23,7 @@ public class BookCaseIdleState : IState<BookCase>
         {
             foreach (var hit in BoxHits)
             {
-                if (hit.collider.bounds.min.y > context.BookCaseBotton)
+                if (hit.collider.bounds.min.y > context.BookCaseBottom)
                     context.SetState(new BookCasePushState());
                 return;
             }
--- Assets/Scripts/Trap/BookCase/BookCaseState/BookCasePushState.cs
+++ Assets/Scripts/Trap/BookCase/BookCaseState/BookCasePushState.cs
@@ -1,26 +1,19 @@
-﻿
-
-using System;
-using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-using static UnityEngine.UI.Image;
+﻿using UnityEngine;
 
 public class BookCasePushState : IState<BookCase>
 {
     public void Enter(BookCase context)
     {
-        Debug.Log($"{this}진입");
     }
 
     public void Exit(BookCase context)
     {
-        Debug.Log($"{this}탈출");
     }
 
     public void Execute(BookCase context)
     {
         
-        RaycastHit2D leftHits = Physics2D.Raycast(context.Dir(), Vector2.down, 2f, 1 << 31);
+        RaycastHit2D leftHits = Physics2D.Raycast(context.Dir(), Vector2.down, 0.1f, 1 << 31);
 
         if (!leftHits)
         {
@@ -28,7 +21,7 @@ public class BookCasePushState : IState<BookCase>
         }
         else
         {
-            
+            Debug.Log(leftHits.collider);
             context.transform.position = Vector2.MoveTowards(context.transform.position, context.transform.position + context.MoveDir(true), context.Speed * Time.deltaTime);
         }
     }
