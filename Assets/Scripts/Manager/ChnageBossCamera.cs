d01fcd17895ddb871eafc0458f0e52ea277360e8
GodKong2431 <kizna10231@gmail.com>
2025-12-28 17:54:06 +0900

    [fix] 카메라 버그 수정 및 애니메이션 수정
    카메라 버그 수정 및 애니메이션 수정

--- Assets/Scripts/Manager/ChnageBossCamera.cs
+++ Assets/Scripts/Manager/ChnageBossCamera.cs
@@ -7,15 +7,21 @@ public class ChnageBossCamera : MonoBehaviour
     [SerializeField] private CinemachineCamera _bossCam;
 
 
-    public void ChangeChamera()
+    public void ChangeBossChamera()
     {
         if(!_bossCam.gameObject.activeSelf)
             _bossCam.gameObject.SetActive(true);
 
+        _stageCam.Priority = 0;
+        _bossCam.Priority = 10;
+    }
 
-        int temp = _stageCam.Priority;
-        _stageCam.Priority = _bossCam.Priority;
-        _bossCam.Priority = temp;
+    public void ChageStageCamera()
+    {
+        if (_bossCam.gameObject.activeSelf)
+            _bossCam.gameObject.SetActive(false);
 
+        _stageCam.Priority = 10;
+        _bossCam.Priority = 0;
     }
 }
